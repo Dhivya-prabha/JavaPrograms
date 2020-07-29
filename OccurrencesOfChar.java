18e76eea5fbef4bf65fdbbc29322da49ce71fc19
Dhivya-prabha <49254873+Dhivya-prabha@users.noreply.github.com>
2020-07-29 13:53:18 +0530

    Add files via upload

---
+++ OccurrencesOfChar.java
@@ -0,0 +1,29 @@
+package Learning;
+
+
+public class OccurrencesOfChar {
+public static void main(String[] args) {
+
+
+
+			// Check number of occurrences of a char (eg 'e') in a String
+			
+			String str = "welcome to chennai";
+			char[] charArray = str.toCharArray();
+			
+			int length = str.length();
+			int count = 0;
+			
+			for(int i = 0;i<length;i++)
+			{
+				if(charArray[i] == 'e')
+				{
+					count = count + 1;
+					
+				}
+				
+			}
+	System.out.println(count);
+		}
+
+	}
