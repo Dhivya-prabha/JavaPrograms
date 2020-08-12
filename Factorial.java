4ec396aa06016273237d7a44e3cdb640549169a3
Dhivya-prabha <49254873+Dhivya-prabha@users.noreply.github.com>
2020-08-12 19:16:10 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+package Salesforce.practice;
+
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+
+		
+		int fact = 1, input = 5;
+		
+		
+		for(int i=1;i<=input;i++)
+		{
+			fact = fact*i;
+			
+		}System.out.println(fact);
+		
+	
+	}
+
+}
